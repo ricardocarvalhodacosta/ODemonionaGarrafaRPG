08a2ab2f7239248afaa62c8a82c9f71b4c976752
Ricardo Carvalho da Costa <115588683+ricardocarvalhodacosta@users.noreply.github.com>
2023-04-06 21:17:03 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -3,10 +3,26 @@
 #include <string.h>
 #include <ctype.h>
 #include <ncurses.h>
+#include <locale.h>
 #include "header.h"
 
 int main () {
+
+    setlocale(LC_ALL, "portuguese-brazilian"); 
+    initscr();
+    clear();
+    noecho();
+    cbreak();
+    curs_set(0);
+    start_color();
+    init_pair(1, COLOR_RED, COLOR_WHITE);
+    init_pair(2, COLOR_WHITE, COLOR_RED);
+
     menu();
     prologo();
+
+    refresh();
+    getch();
+    endwin();
     return(0);
 }
